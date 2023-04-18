49560de527e46be2821a38dbb8205528d32bf697
fenyadim <dimos9813@gmail.com>
2023-04-18 16:53:56 +0800

    Settings

--- next.config.js
+++ next.config.js
@@ -1,37 +1,38 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
+const path = require('path')
 
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    HOST_BACK: '127.0.0.1',
-    PORT_BACK: '3001',
-    URL_BACK: `http://127.0.0.1:3001`,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '3001',
-      },
-    ],
-    path: '/',
-  },
-  sassOptions: {
-    fiber: false,
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/kosmetika',
-        permanent: true,
-      },
-    ];
-  },
-};
+	reactStrictMode: true,
+	poweredByHeader: false,
+	env: {
+		HOST_BACK: '127.0.0.1',
+		PORT_BACK: '3001',
+		URL_BACK: `http://127.0.0.1:3001`,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: '127.0.0.1',
+				port: '3001',
+			},
+		],
+		path: '/',
+	},
+	sassOptions: {
+		fiber: false,
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/kosmetika',
+				permanent: true,
+			},
+		]
+	},
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
