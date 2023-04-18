1f12a34eb2532e120caf40a6c6d616c797f6961c
fenyadim <dimos9813@gmail.com>
2023-04-18 13:11:50 +0800

    Refactoring

--- next.config.js
+++ next.config.js
@@ -1,30 +1,37 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-        HOST_BACK: "127.0.0.1",
-        PORT_BACK: "3001",
-        URL_BACK: `http://127.0.0.1:3001`,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '3001'
-            },
-        ],
-        path: '/'
-    },
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/kosmetika",
-                permanent: true
-            },
-        ];
-    },
+  reactStrictMode: true,
+  env: {
+    HOST_BACK: '127.0.0.1',
+    PORT_BACK: '3001',
+    URL_BACK: `http://127.0.0.1:3001`,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3001',
+      },
+    ],
+    path: '/',
+  },
+  sassOptions: {
+    fiber: false,
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/kosmetika',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
