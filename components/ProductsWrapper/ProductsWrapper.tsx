95c5f5bfbc739f5e536d498ec5ffaf3d9a43886c
fenyadim <dimos9813@gmail.com>
2023-04-20 08:46:48 +0800

    Refactoring

--- components/ProductsWrapper/ProductsWrapper.tsx
+++ components/ProductsWrapper/ProductsWrapper.tsx
@@ -1,12 +1,26 @@
-import { FC, Fragment } from 'react'
+import { FC, Fragment, useEffect, useRef } from 'react'
 
 import { Header, ProductItem, TabsWrapper } from '@/components'
+
+import { IBasketData, getBasket } from '@/redux/basketSlice'
+import { useAppSelector } from '@/redux/hooks'
 
 import { IProductsData } from '@/types'
 
 import styles from './ProductsWrapper.module.scss'
 
 const ProductsWrapper: FC<IProductsData> = ({ products, tabs }) => {
+	const basket: IBasketData[] = useAppSelector(getBasket)
+	const isMounted = useRef(false)
+
+	useEffect(() => {
+		if (isMounted) {
+			const jsonBasket = JSON.stringify(basket)
+			localStorage.setItem('basket', jsonBasket)
+		}
+		isMounted.current = true
+	}, [basket])
+
 	return (
 		<>
 			<Header>
